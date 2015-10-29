b3da5f547720c6727f74b61cde57f5e7db89fd91
ephilippona <ephilippona@gmail.com>
2015-10-29 18:53:42 -0400

    Initial Commit

---
+++ src/Survey.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.security.cert.Certificate;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Survey {
+
+	public static void main(String[] args) {
+		
+		//ArrayList<Row> websites = new ArrayList<Row>() ;
+		
+		//SSLSocket socket;
+		
+		//for(Row r: websites){
+			
+			
+		//}
+		
+		
+		
+		
+	}
+
+}
